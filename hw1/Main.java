03265b4d3c2626a557f9dbf40d3a211ce9bf3d35
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-05-21 17:39:53 +0300

    add Main.java

---
+++ hw1/Main.java
@@ -0,0 +1,20 @@
+package hw1;
+
+// Домашнее задание к семинару от 20.05.2023
+
+public class Main {
+    public static void main(String[] args) {
+    
+        // Task1
+        //Task1 task1 = new Task1();
+        //task1.сalculate(task1.inputNumber());
+        
+        // Task2
+        //Task2 task2 = new Task2();
+        //task2.elementalNumbers();
+
+        // Task3
+        Task3 task3 = new Task3();
+        task3.сalculate();
+    }
+}
