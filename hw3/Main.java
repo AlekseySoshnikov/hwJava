e59aad8577a89ae91542ecc5d688587f8575c32f
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-05-27 17:11:22 +0300

    add Main.java

---
+++ hw3/Main.java
@@ -0,0 +1,8 @@
+// Домашнее задание к семинару от 26.05.2023
+
+public class Main {
+    public static void main(String[] args) {
+        Task task = new Task();
+        task.сalculate();
+    }    
+}
