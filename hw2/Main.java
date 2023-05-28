f6e55bc99d997e7f8d42fff6feeee8244b43c019
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-05-28 16:37:25 +0300

    add Main.java

---
+++ hw2/Main.java
@@ -0,0 +1,13 @@
+//Домашнее задание к семинару от 23.05.2023
+
+public class Main {
+    public static void main(String[] args) {
+        // Task1
+        // Task1 task1 = new Task1();
+        // System.out.println(task1.buildStr());
+
+        // Task3
+        // Task3 task3 = new Task3();
+        // task3.buildStr();
+    }
+}
