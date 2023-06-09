31f01eaba8f064233a34a5003978f4bea8c69047
Aleksey_Soshnikov <aasoshnikov@yandex.ru>
2023-06-09 17:12:02 +0300

    add MyComparator.java

---
+++ hw5/MyComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ArrayList;
+
+public class MyComparator implements Comparator<Map.Entry<String, ArrayList<String>>> {
+    @Override
+    public int compare(Map.Entry<String, ArrayList<String>> o1, Map.Entry<String, ArrayList<String>> o2) {
+        if (o1.getValue().size() > o2.getValue().size()) return -1;
+        if (o1.getValue().size() < o2.getValue().size()) return 1;
+        else return 0;
+    }
+    
+}
